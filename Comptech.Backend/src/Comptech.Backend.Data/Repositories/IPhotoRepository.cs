61fedfe090f0f440d2d57b39935c7648ff574b44
Andrey Kaynara <andrey.kaynara@yandex.ru>
2017-02-04 20:05:20 +0700

    Repository interfaces added

---
+++ Comptech.Backend/src/Comptech.Backend.Data/Repositories/IPhotoRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Comptech.Backend.Data.DomainEntities;
+
+namespace Comptech.Backend.Data.Repositories
+{
+    public interface IPhotoRepository : IRepository<Photo>
+    {
+        Photo GetLastPhotoInSession(int sessionId);
+        int GetLastPhotoIdInSession(int sessionId);
+    }
+
+}
