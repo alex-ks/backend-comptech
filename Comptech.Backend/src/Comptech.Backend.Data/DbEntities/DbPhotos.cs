8db6e657a03aa187a54a750ede5fac0438c1fa47
disbolans <le.li4@ya.ru>
2017-02-04 12:56:25 +0700

    Db entities added

---
+++ Comptech.Backend/src/Comptech.Backend.Data/DbEntities/DbPhotos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Comptech.Backend.Data.DbEntities
+{
+    public class DbPhotos
+    {
+    }
+}
