7226a078304405d399ae9b3391503f68e363814d
Макалев Андрей Игоревич <green124b@gmail.com>
2017-02-04 19:19:05 +0700

    Created method accepting photo request from frontend

--- Comptech.Backend/src/Comptech.Backend.Service/Controllers/PhotoController.cs
+++ Comptech.Backend/src/Comptech.Backend.Service/Controllers/PhotoController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Comptech.Backend.Service.Data;
+using Comptech.Backend.Service.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging; 
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +11,28 @@ using System.Threading.Tasks;
 
 namespace Comptech.Backend.Service.Controllers
 {
+    [Authorize]
     public class PhotoController : Controller
     {
+        private readonly ILogger logger;
+        private readonly UserManager<ApplicationUser> userManager;
+        
+        public PhotoController(UserManager<ApplicationUser> userManager,
+                                ILoggerFactory loggerFactory)
+        {
+            this.userManager = userManager;
+            logger = loggerFactory.CreateLogger<UserController>();
+        }
+
+        [Route("rest/faces")]
+        [HttpGet]
+        public IActionResult GetPhoto()
+        {
+            using (logger.BeginScope(nameof(GetPhoto)))
+            {
+                logger.LogInformation("Send Photo");
+                return Ok();
+            }
+        } 
     }
 }
