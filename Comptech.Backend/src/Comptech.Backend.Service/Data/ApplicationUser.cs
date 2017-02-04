3ad892c21f7507edaafe76cbacddb5244d4fb8e7
Alexey <eliseev-eag@yandex.ru>
2017-02-04 17:50:30 +0700

    Project template created

---
+++ Comptech.Backend/src/Comptech.Backend.Service/Data/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using OpenIddict;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Comptech.Backend.Service.Data
+{
+    public class ApplicationUser : OpenIddictUser<int>
+    {
+        
+    }
+}
