260c88fdec512bde22ee83e2be7a045e9d07f59e
disbolans <le.li4@ya.ru>
2017-02-04 12:38:51 +0700

    Domain entities added

---
+++ Comptech.Backend/src/Comptech.Backend.Data/DomainEntities/Photo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Comptech.Backend.Data.DomainEntities
+{
+    public class Photo
+    {
+        public int PhotoID { get; set; }
+        public int SessionID { get; set; }
+        public byte[] Image { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
---
+++ Comptech.Backend/src/Comptech.Backend.Data/DomainEntities/RecognitionResults.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Comptech.Backend.Data.DomainEntities
+{
+    public class RecognitionResults
+    {
+        public bool IsValid { get; set; }
+        public Points Coords { get; set; }
+        public int PhotoID { get; set; }
+
+    }
+
+}
