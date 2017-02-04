cabbe18d0b67648b8773edc3cbf018bf88d25cd8
Alexey <eliseev-eag@yandex.ru>
2017-02-04 18:58:24 +0700

    Dummy PulseController created

--- Comptech.Backend/src/Comptech.Backend.Service/Controllers/PulseController.cs
+++ Comptech.Backend/src/Comptech.Backend.Service/Controllers/PulseController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Comptech.Backend.Service.Data;
+using Comptech.Backend.Service.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +11,27 @@ using System.Threading.Tasks;
 
 namespace Comptech.Backend.Service.Controllers
 {
+    [Authorize]
     public class PulseController : Controller
     {
+        private readonly ILogger logger;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public PulseController(UserManager<ApplicationUser> userManager, ILoggerFactory loggerFactory)
+        {
+            this.userManager = userManager;
+            logger = loggerFactory.CreateLogger<PulseController>();
+        }
+
+        [Route("rest/bpm")]
+        [HttpPost]
+        public IActionResult AcceptPulseFromMobile([FromBody] AcceptPulseRequest request)
+        {
+            using (logger.BeginScope(nameof(AcceptPulseFromMobile)))
+            {
+                logger.LogInformation("Pulse accepted");
+                return Ok();
+            }
+        }
     }
 }
---
+++ Comptech.Backend/src/Comptech.Backend.Service/Models/AcceptPulseRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace Comptech.Backend.Service.Models
+{
+    public class AcceptPulseRequest
+    {
+        [DataMember(Name = "bpm")]
+        public int Pulse { get; set; }
+        [DataMember(Name = "timestamp")]
+        public DateTime TimeStamp { get; set; }
+    }
+}
