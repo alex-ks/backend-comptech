2af80d9281ee6a30ee21712212ec575794ddb213
Lev Sizov <levsizov2015@outlook.com0>
2017-02-04 19:05:02 +0700

    Add SessionController and FinishRequest.
    Add stub method FinishSession, which returns 200 OK

---
+++ Comptech.Backend/src/Comptech.Backend.Service/Controllers/SessionController.cs
@@ -0,0 +1,36 @@
+﻿using Comptech.Backend.Service.Data;
+using Comptech.Backend.Service.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Comptech.Backend.Service.Controllers
+{
+    [Authorize]
+    public class SessionController : Controller
+    {
+        private readonly ILogger logger;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public SessionController(UserManager<ApplicationUser> userManager,
+                               ILoggerFactory loggerFactory)
+        {
+            this.userManager = userManager;
+            logger = loggerFactory.CreateLogger<UserController>();
+        }
+
+        [Route("/rest/finished")]
+        [HttpPost]
+        public async Task<IActionResult> FinishSession([FromBody] FinishRequest request)
+        {
+            using (logger.BeginScope(nameof(FinishSession)))
+            {
+                //TODO: Добавить логику завершения сессии
+
+                return Ok();
+            }
+        }
+    }
+}
---
+++ Comptech.Backend/src/Comptech.Backend.Service/Models/FinishRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace Comptech.Backend.Service.Models
+{
+    public class FinishRequest
+    {
+        [DataMember(Name = "finished")]
+        public int Finished { get; set; }
+
+        [DataMember(Name = "sessionId")]
+        public string SessionId { get; set; }
+    }
+} 
