72ba72897bd1eae9094801168b32b1ed14cea9e7
Alexey <eliseev-eag@yandex.ru>
2017-02-05 15:24:51 +0700

    Added check for active session Added check for active session on AcceptPulse method on PulseController Added sessionLenght on appsettings.json

--- Comptech.Backend/src/Comptech.Backend.Service/Controllers/PulseController.cs
+++ Comptech.Backend/src/Comptech.Backend.Service/Controllers/PulseController.cs
@@ -5,6 +5,7 @@ using Comptech.Backend.Service.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -29,9 +30,10 @@ namespace Comptech.Backend.Service.Controllers
 
         [Route("rest/bpm")]
         [HttpPost]
-        public async Task<IActionResult> AcceptPulseFromMobile([FromBody] AcceptPulseRequest request)
+        public async Task<IActionResult> AcceptPulse([FromBody] AcceptPulseRequest request,
+                                                     [FromServices] IConfiguration configuration)
         {
-            using (logger.BeginScope(nameof(AcceptPulseFromMobile)))
+            using (logger.BeginScope(nameof(AcceptPulse)))
             {
                 logger.LogInformation("Pulse accepted");
                 logger.LogInformation("User tries to save pulse");
@@ -40,13 +42,34 @@ namespace Comptech.Backend.Service.Controllers
                     var user = await userManager.GetUserAsync(HttpContext.User);
                     var userId = await userManager.GetUserIdAsync(user);
                     var session = sessionRepository.GetLastSessionForUser(int.Parse(userId));
-                    Pulse pulse = new Pulse();
-                    pulse.SessionID = session.SessionID;
-                    pulse.TimeStamp = request.TimeStamp;
-                    pulse.BPM = request.Pulse;
-                    pulseRepository.Add(pulse);
-                    logger.LogInformation("User {0} sent pulse",userId);
-                    return Ok();
+                    if (session.Status == SessionStatus.ACTIVE)
+                    {
+                        var sessionLenght = TimeSpan.FromSeconds(int.Parse(
+                                configuration.GetSection("SessionLenght").Value));
+                        if ((DateTime.Now - session.Start) < sessionLenght)
+                        {
+                            Pulse pulse = new Pulse();
+                            pulse.SessionID = session.SessionID;
+                            pulse.TimeStamp = request.TimeStamp;
+                            pulse.BPM = request.Pulse;
+                            pulseRepository.Add(pulse);
+                            logger.LogInformation("User {0} sent pulse", userId);
+                            return Ok();
+                        }
+                        else
+                        {
+                            logger.LogInformation("Users {0} session is ended", userId);
+                            session.Status = SessionStatus.FINISHED;
+                            sessionRepository.Update(session);
+                            return Forbid();
+                        }
+                    }
+                    else
+                    {
+                        logger.LogInformation("Users {0} session is ended", userId);
+                        return Forbid();
+                    }
+
                 }
                 catch (Exception exception)
                 {
@@ -55,5 +78,15 @@ namespace Comptech.Backend.Service.Controllers
                 }
             }
         }
+        /*
+        [Route("rest/pulse")]
+        [HttpGet]
+        public async Task<IActionResult> SendPulse()
+        {
+            using (logger.BeginScope(nameof(SendPulse)))
+            {
+                logger.LogInformation("Pulse sent");
+            }
+        }*/
     }
 }
