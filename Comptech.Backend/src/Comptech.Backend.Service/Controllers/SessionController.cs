56499be4ebe9f2823f46942a9ac164b2b73d34af
Lev Sizov <levsizov2015@outlook.com>
2017-02-05 20:18:36 +0700

    Add Exception, BadRequests message
    After-review changes. Add Exception handling - log message and stack
    trace and send back BadRequest with exception message. Add error
    messages to BadRequest response body.

--- Comptech.Backend/src/Comptech.Backend.Service/Controllers/SessionController.cs
+++ Comptech.Backend/src/Comptech.Backend.Service/Controllers/SessionController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
-
+using System;
 
 namespace Comptech.Backend.Service.Controllers
 {
@@ -34,37 +34,58 @@ namespace Comptech.Backend.Service.Controllers
         [HttpPost]
         public async Task<IActionResult> FinishSession([FromBody] FinishRequest request)
         {
-            using (logger.BeginScope(nameof(FinishSession)))
+            try
             {
-                //если в запросе нет id сессии, то возвращаем ошибку(?)
-                if (request.SessionId == null)
+                using (logger.BeginScope(nameof(FinishSession)))
                 {
-                    logger.LogError($"Attempted to finish session with null sessionId");
-                    return BadRequest();
+                    //если в запросе нет id сессии, то возвращаем ошибку(?)
+                    if (request.SessionId == null)
+                    {
+                        logger.LogError($"Attempted to finish session with null sessionId");
+                        return BadRequest(new
+                        {
+                            message = "Error: Session ID is empty"
+                        });
+                    }
+
+                    var user = await userManager.GetUserAsync(HttpContext.User);
+                    var userId = await userManager.GetUserIdAsync(user);
+
+                    var session = sessionRepository.GetLastSessionForUser(int.Parse(userId));
+                    //проверяем несовпадение переданного id сессии и id сессии в базе
+                    if (session.SessionID != request.SessionId)
+                    {
+                        logger.LogCritical($"User {userId}: Obtained session ID is incorrect or malformed.");
+                        return BadRequest(new
+                        {
+                            message = "Error: Authentification failure. Your session ID is incorrect of malformed."
+                        });
+                    }
+
+                    //если сессия уже почему-то завершена, то возвращаем 409 Conflict
+                    if (session.Status == SessionStatus.FINISHED)
+                    {
+                        logger.LogWarning($"User {userId}: Session is already finished.");
+                        return StatusCode(409, new
+                        {
+                            message = "Error: Session is already finished."
+                        }); //Conflict
+                    }
+                    //обновляем статус
+                    session.Status = SessionStatus.FINISHED;
+                    repository.Update(session);
+
+                    return Ok();
                 }
-
-                var user = await userManager.GetUserAsync(HttpContext.User);
-                var userId = await userManager.GetUserIdAsync(user);
-
-                var session = sessionRepository.GetLastSessionForUser(int.Parse(userId));
-                //проверяем несовпадение переданного id сессии и id сессии в базе
-                if(session.SessionID != request.SessionId)
+            }
+            catch(Exception e)
+            {
+                logger.LogCritical(e.Message);
+                logger.LogTrace(e.StackTrace);
+                return BadRequest(new
                 {
-                    logger.LogCritical($"User {userId}: Obtained session ID is incorrect or malformed");
-                    return BadRequest();
-                }
-
-                //если сессия уже почему-то завершена, то возвращаем 409 Conflict
-                if(session.Status == SessionStatus.FINISHED)
-                {
-                    logger.LogWarning($"User {userId}: Session is already finished");
-                    return StatusCode(409); //Conflict
-                }
-                //обновляем статус
-                session.Status = SessionStatus.FINISHED;
-                repository.Update(session);
-
-                return Ok();
+                    message = e.Message
+                });
             }
         }
     }
