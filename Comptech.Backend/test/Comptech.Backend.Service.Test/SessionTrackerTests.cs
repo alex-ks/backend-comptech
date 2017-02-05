288847534e5f556d85f238652f62ffa855566fc4
Alisa Obukhova <obukhova.alisa@gmail.com>
2017-02-05 12:49:13 +0700

    Added SessionTrackerTests

---
+++ Comptech.Backend/test/Comptech.Backend.Service.Test/SessionTrackerTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Comptech.Backend.Service.Test
+{
+    public class SessionTrackerTests
+    {
+        private AspApplicationMock app;
+        public SessionTrackerTests()
+        {
+            var config = new Dictionary<string, string>()
+            {
+                ["SessionTimeout"] = "\"00:01:00\"",
+                ["TimeoutCheckInterval"] = "\"00:00:01\""
+            };
+        }
+
+        [Fact]
+        public void CanStartSession()
+        {
+            //Arrange
+            var sessionTracker = new SessionTracker()
+    
+            //Act
+
+            //Assert
+
+        }
+    }
+}
