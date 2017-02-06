f9414c4ae70a22cf6dc00ee77e9528665e089046
Lev Sizov <levsizov2015@outlook.com>
2017-02-06 11:29:40 +0700

    Comments for public component RecognitionTaskQueue
    Added comments with summary and usage example to RecognitionTaskQueue
    and public methods

--- Comptech.Backend/src/Comptech.Backend.Service/RecognitionTaskQueue.cs
+++ Comptech.Backend/src/Comptech.Backend.Service/RecognitionTaskQueue.cs
@@ -5,6 +5,24 @@ using System.Collections.Concurrent;
 
 namespace Comptech.Backend.Service
 {
+    /// <summary>
+    /// RecognitionTaskQueue which handles recognition tasks sent to analytics by adding
+    /// <code>RecognitionTask</code> to queue and removing from it
+    /// </summary>
+    /// <example>
+    /// Adding task to queue:
+    /// <code>
+    /// RecognitionTask task = new RecognitionTask();
+    /// ... //fill in RecognitionTask properties
+    /// RecognitioTaskQueue queue = ... //get RecognitionTaskQueue from DI
+    /// queue.Enqueue(task);
+    /// </code>
+    /// Getting task from queue:
+    /// <code>
+    /// RecognitioTaskQueue queue = ... //get RecognitionTaskQueue from DI
+    /// RecognitionTask task = queue.Dequeue();
+    /// </code>
+    /// </example>
     public class RecognitionTaskQueue
     {
         private volatile BlockingCollection<RecognitionTask> queue = new BlockingCollection<RecognitionTask>(
@@ -12,11 +30,20 @@ namespace Comptech.Backend.Service
             );
         private readonly ILogger logger;
 
+        /// <summary>
+        /// Basic constructor called by DI
+        /// </summary>
+        /// <param name="loggerFactory"></param>
         public RecognitionTaskQueue(ILoggerFactory loggerFactory)
         {
             logger = loggerFactory.CreateLogger<RecognitionTaskQueue>();
         }
 
+        /// <summary>
+        /// Removes RecognitionTask from queue and returns it
+        /// May hang up if queue is empty until at least one task is in queue
+        /// </summary>
+        /// <returns>RecognitionTask - recognition task for analytics service</returns>
         public RecognitionTask Dequeue()
         {
             RecognitionTask task;
@@ -24,6 +51,10 @@ namespace Comptech.Backend.Service
             return task;
         }
 
+        /// <summary>
+        /// Adds recognition task to recognition queue for analytics service
+        /// </summary>
+        /// <param name="task">RecognitionTask - recognition task for analytics service</param>
         public void Enqueue(RecognitionTask task)
         {
             queue.TryAdd(task);
--- Comptech.Backend/src/Comptech.Backend.Service/Startup.cs
+++ Comptech.Backend/src/Comptech.Backend.Service/Startup.cs
@@ -51,6 +51,7 @@ namespace Comptech.Backend.Service
 
             services.AddMvc();
 
+            //добавляем TaskRecognitionQueue в DI как сиглтон
             services.AddSingleton<RecognitionTaskQueue>();
         }
 
