425165093cd5316ac1693bd049b23cefa2f3e065
Макалев Андрей Игоревич <green124b@gmail.com>
2017-02-06 21:00:09 +0700

    Fixed typos

--- Comptech.Backend/test/Comptech.Backend.Service.Test/AnalyticsClientTest.cs
+++ Comptech.Backend/test/Comptech.Backend.Service.Test/AnalyticsClientTest.cs
@@ -9,33 +9,28 @@ namespace Comptech.Backend.Service.Test
 {
     public class AnalyticsClientTest
     {
-        private IAnalyticsClient client = new AnalyticsClient("94.180.119.78");
+        private IAnalyticsClient client = new AnalyticsClient("http://94.180.119.78/");
         private string modelName = "model1";
         private int photoId = 1;
         private string sessionId = null;
-        private ILogger logger;
 
         [Fact]
         public async Task AnalyticsClientTestMethod()
         {
-            using (logger.BeginScope(nameof(AnalyticsClientTestMethod)))
-            {
-                    while (null == sessionId)
-                    {   
-                        sessionId = await client.RequestRecognitionSession(modelName);
-                    }
-                    
-                    logger.LogInformation("sessionId = {0}", sessionId);
-                    
-                    await client.UploadPhoto(new byte[10], sessionId);
-                    
-                    var recRes = await client.TryGetResults(sessionId, photoId);
-
-                    while (null == recRes)
-                    {
-                        recRes = await client.TryGetResults(sessionId, photoId);
-                    } 
+            while (null == sessionId)
+            {   
+                sessionId = await client.RequestRecognitionSession(modelName);
             }
+            
+            await client.UploadPhoto(new byte[10], sessionId);
+            
+            var recRes = await client.TryGetResults(sessionId, photoId);
+
+            while (null == recRes)
+            {
+                recRes = await client.TryGetResults(sessionId, photoId);
+            } 
+        }
         }
     }
 }
