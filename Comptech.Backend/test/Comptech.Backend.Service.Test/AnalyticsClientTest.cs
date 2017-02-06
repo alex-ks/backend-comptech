3af18f0612e12f94966fb05e3b9b25be8b9e025b
Макалев Андрей Игоревич <green124b@gmail.com>
2017-02-06 20:01:57 +0700

    Added integration testing for AnalyticsAgent

--- Comptech.Backend/test/Comptech.Backend.Service.Test/AnalyticsClientTest.cs
+++ Comptech.Backend/test/Comptech.Backend.Service.Test/AnalyticsClientTest.cs
@@ -1,12 +1,41 @@
 using Comptech.Backend.Service.Models;
 using Comptech.Backend.Service.Analytics;
 using Microsoft.Extensions.Logging;
+using System;
 using Xunit;
+using System.Threading.Tasks;
 
 namespace Comptech.Backend.Service.Test
 {
     public class AnalyticsClientTest
     {
         private IAnalyticsClient client = new AnalyticsClient("94.180.119.78");
+        private string modelName = "model1";
+        private int photoId = 1;
+        private string sessionId = null;
+        private ILogger logger;
+
+        [Fact]
+        public async Task AnalyticsClientTestMethod()
+        {
+            using (logger.BeginScope(nameof(AnalyticsClientTestMethod)))
+            {
+                    while (null == sessionId)
+                    {   
+                        sessionId = await client.RequestRecognitionSession(modelName);
+                    }
+                    
+                    logger.LogInformation("sessionId = {0}", sessionId);
+                    
+                    await client.UploadPhoto(new byte[10], sessionId);
+                    
+                    var recRes = await client.TryGetResults(sessionId, photoId);
+
+                    while (null == recRes)
+                    {
+                        recRes = await client.TryGetResults(sessionId, photoId);
+                    } 
+            }
+        }
     }
 }
