109cf9d5910f45d110f65f23d8ab44951942cbfd
disbolans <le.li4@ya.ru>
2017-02-06 08:25:15 +0700

    test start

--- Comptech.Backend/Comptech.Backend.Data.Tests/Class.cs
+++ Comptech.Backend/Comptech.Backend.Data.Tests/Class.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace Comptech.Backend.Data.Tests
+namespace Comptech.Backend.Data.Test
 {
-    
     public class Class
     {
+        [Fact]
+
+
     }
 }
