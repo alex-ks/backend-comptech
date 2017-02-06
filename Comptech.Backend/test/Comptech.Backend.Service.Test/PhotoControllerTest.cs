711e1fb4c779da6891e7c6c36183f26a80bb87da
Макалев Андрей Игоревич <green124b@gmail.com>
2017-02-07 04:01:56 +0700

    Added checking the validity of the answer

--- Comptech.Backend/test/Comptech.Backend.Service.Test/PhotoControllerTest.cs
+++ Comptech.Backend/test/Comptech.Backend.Service.Test/PhotoControllerTest.cs
@@ -12,12 +12,16 @@ using Moq;
 using Comptech.Backend.Data.Repositories;
 using System.Linq;
 using Comptech.Backend.Data.DomainEntities;
+using System;
 
 namespace Comptech.Backend.Service.Test
 {
     public class PhotoControllerTest
     {
         private AspApplicationMock app;
+        private byte[] image;
+        private int photoId = 2;
+        private RecognitionResults testResults;
 
         public PhotoControllerTest()
         {
@@ -41,34 +45,119 @@ namespace Comptech.Backend.Service.Test
             };
         }
 
-        [Theory]
-        private async Task TestGetPhoto(Session testSession,
-                                        Photo testPhoto,
-                                        RecognitionResults testRecognitionResults)
+        [Fact]
+        private async Task TestGetPhoto()
         {
-            var repository = new MockRepository(MockBehavior.Default);
-
-            ISessionRepository sessionRepository = repository.Of<ISessionRepository>()
-                .Where(sr => sr.GetLastSessionForUser(It.IsAny<int>()) == testSession)
-                .First();
-
-            IPhotoRepository photoRepository = repository.Of<IPhotoRepository>()
-                .Where(pr => pr.GetLastPhotoInSession(It.IsAny<int>()) == testPhoto)
-                .First();
-                
-            IRecognitionResultsRepository recognitionResultsRepository = repository.Of<IRecognitionResultsRepository>()
-                .Where(rr => rr.GetRecognitionResultsByPhotoId(It.IsAny<int>()) == testRecognitionResults)
-                .First();
-
-
             var user = new ApplicationUser { UserName = "UserTest", Email = "test@mail.ru" };
             await app.UserManager.CreateAsync(user, "UserTest@123");
             await app.SetUser(user);
 
+            ISessionRepository sessionRepository;
+            IPhotoRepository photoRepository;
+            IRecognitionResultsRepository recognitionResultsRepository;
+
+            InstantiateRepositories(user, out sessionRepository, out photoRepository, out recognitionResultsRepository);
+
             PhotoController controller = CreateController();
 
             var result = (await controller.GetPhoto(photoRepository,
-                         sessionRepository, recognitionResultsRepository) as OkObjectResult).Value; 
-        } 
+                         sessionRepository, recognitionResultsRepository) as OkObjectResult).Value;
+
+            var resultPhoto = result.GetType().GetProperty("photo").GetValue(result) as string;
+            var recognitionResults = getRecognitionResultsFromJsonResults(result, photoId);
+
+            Assert.Equal(resultPhoto, Convert.ToBase64String(image));
+            Assert.Equal(testResults.IsValid, recognitionResults.IsValid);
+            Assert.Equal(testResults.PhotoID, recognitionResults.PhotoID);
+            Assert.Equal(testResults.Coords.TopLeft.X, recognitionResults.Coords.TopLeft.X);
+            Assert.Equal(testResults.Coords.TopLeft.Y, recognitionResults.Coords.TopLeft.Y);
+            Assert.Equal(testResults.Coords.BottomRight.X, recognitionResults.Coords.BottomRight.X);
+            Assert.Equal(testResults.Coords.BottomRight.Y, recognitionResults.Coords.BottomRight.Y);
+        }
+        
+        private void InstantiateRepositories(ApplicationUser user,
+                                             out ISessionRepository sessionRepository,
+                                             out IPhotoRepository photoRepository,
+                                             out IRecognitionResultsRepository recognitionResultsRepository)
+        {
+            var repository = new MockRepository(MockBehavior.Default);
+
+            image = new byte[] { 0x20, 0x20, 0x20 };
+            testResults = new RecognitionResults(true, new Points(new Point(0,1), new Point(2,3)), photoId);
+            
+            var timeStamp = new DateTime();
+            var userId = user.Id;
+
+            Session testSession = new Session(user.Id, timeStamp, timeStamp, SessionStatus.ACTIVE);
+            testSession.SessionID = 1;
+            
+            Photo testPhoto = new Photo(testSession.SessionID, image, timeStamp);
+            testPhoto.PhotoID = 2;
+
+
+            sessionRepository = repository.Of<ISessionRepository>()
+                .Where(sr => sr.GetLastSessionForUser(It.IsAny<int>()) == testSession)
+                .First();
+
+            photoRepository = repository.Of<IPhotoRepository>()
+                .Where(pr => pr.GetLastPhotoInSession(It.IsAny<int>()) == testPhoto)
+                .First();
+                
+            recognitionResultsRepository = repository.Of<IRecognitionResultsRepository>()
+                .Where(rr => rr.GetRecognitionResultsByPhotoId(It.IsAny<int>()) == testResults)
+                .First();
+        }
+
+        private RecognitionResults getRecognitionResultsFromJsonResults(Object result, int photoId)
+        {
+            Object recognitionResults = result.GetType().GetProperty("recognitionResults").GetValue(result) as Object;
+
+            if (null == recognitionResults) return null;
+
+            bool? isValid = recognitionResults
+                            .GetType()
+                            .GetProperty("valid")
+                            .GetValue(recognitionResults) as bool?;
+            
+            Object coords = recognitionResults
+                            .GetType()
+                            .GetProperty("coordinates")
+                            .GetValue(recognitionResults) as Object;
+
+            Object topLeftCoords = coords
+                                   .GetType()
+                                   .GetProperty("topLeft")
+                                   .GetValue(coords) as Object;
+
+            Object bottomRightCoords = coords
+                                       .GetType()
+                                       .GetProperty("bottomRight")
+                                       .GetValue(coords) as Object;
+            
+            int? topLeftX = topLeftCoords
+                            .GetType()
+                            .GetProperty("x")
+                            .GetValue(topLeftCoords) as int?;
+
+            int? topLeftY = topLeftCoords
+                            .GetType()
+                            .GetProperty("y")
+                            .GetValue(topLeftCoords) as int?;
+            
+            int? bottomRightX = bottomRightCoords
+                                .GetType()
+                                .GetProperty("x")
+                                .GetValue(bottomRightCoords) as int?;
+
+            int? bottomRightY = bottomRightCoords
+                                .GetType()
+                                .GetProperty("y")
+                                .GetValue(bottomRightCoords) as int?;
+
+            return new RecognitionResults(isValid.Value,
+                                          new Points(new Point(topLeftX.Value, topLeftY.Value),
+                                                     new Point(bottomRightX.Value, bottomRightY.Value)),
+                                          photoId);
+        }
     }
 }
