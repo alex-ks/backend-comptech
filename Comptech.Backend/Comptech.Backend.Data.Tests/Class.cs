6ec59365fe13854287b61e747317e46d5900c3c5
disbolans <le.li4@ya.ru>
2017-02-06 09:13:43 +0700

    Test updated

--- Comptech.Backend/Comptech.Backend.Data.Tests/Class.cs
+++ Comptech.Backend/Comptech.Backend.Data.Tests/Class.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
+using Moq;
+using Comptech.Backend.Data.Repositories.Psql;
 
-namespace Comptech.Backend.Data.Test
+/*namespace Comptech.Backend.Data.Test
 {
     public class Class
     {
         [Fact]
-
-
+            public bool test1()
+        {
+            var mock = new Mock<PsqlContext>();
+            mock.Setup(a=>a.Add().)
     }
+
 }
+}
+*/
