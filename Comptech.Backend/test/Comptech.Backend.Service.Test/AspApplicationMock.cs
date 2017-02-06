400f509eae5b3b9f6a7b6f5e798c76c666ef90bf
Alexander Komissarov <alexander.komissarov@aol.co.uk>
2017-02-06 12:11:50 +0700

    Added ASP.NET classes mocks for test project

---
+++ Comptech.Backend/test/Comptech.Backend.Service.Test/AspApplicationMock.cs
@@ -0,0 +1,36 @@
+﻿using Comptech.Backend.Service.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Comptech.Backend.Service.Test
+{
+    public class AspApplicationMock
+    {
+        public UserManager<ApplicationUser> UserManager { get; set; }
+        public RoleManager<ApplicationRole> RoleManager { get; set; }
+        public SignInManager<ApplicationUser> SignInManager { get; set; }
+        public DefaultHttpContext HttpContext { get; set; }
+        public IOptions<IdentityOptions> IdentityOptions { get; set; }
+        public IServiceProvider ServiceProvider { get; set; }
+
+        public async Task SetUser(ApplicationUser user)
+        {
+            var factory = new UserClaimsPrincipalFactory<ApplicationUser, ApplicationRole>(UserManager,
+                                                                                           RoleManager,
+                                                                                           IdentityOptions);
+            HttpContext.User = await factory.CreateAsync(user);
+        }
+    }
+}
---
+++ Comptech.Backend/test/Comptech.Backend.Service.Test/AspApplicationMockBuilder.cs
@@ -0,0 +1,62 @@
+﻿using Comptech.Backend.Service.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Comptech.Backend.Service.Test
+{
+    public class AspApplicationMockBuilder
+    {
+        public ServiceCollection Services { get; set; }
+
+        public AspApplicationMockBuilder(IDictionary<string, string> configuration)
+        {
+            IConfiguration conf = new ConfigurationBuilder()
+                .AddInMemoryCollection(configuration)
+                .Build();
+
+            Services = new ServiceCollection();
+            Services.AddEntityFramework()
+                    .AddEntityFrameworkInMemoryDatabase()
+                    .AddDbContext<ApplicationDbContext>(opt => opt.UseInMemoryDatabase());
+
+            Services.AddIdentity<ApplicationUser, ApplicationRole>()
+                    .AddEntityFrameworkStores<ApplicationDbContext, int>()
+                    .AddDefaultTokenProviders();
+
+            Services.AddSingleton(conf);
+        }
+
+        public AspApplicationMock Build()
+        {
+            var serviceProvider = Services.BuildServiceProvider();
+            var app = new AspApplicationMock();
+
+            app.HttpContext = new DefaultHttpContext();
+
+            app.HttpContext.Features.Set<IHttpAuthenticationFeature>(new HttpAuthenticationFeature());
+            Services.AddSingleton<IHttpContextAccessor>(h => new HttpContextAccessor
+            {
+                HttpContext = app.HttpContext
+            });
+
+            app.UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            app.RoleManager = serviceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
+            app.SignInManager = serviceProvider.GetRequiredService<SignInManager<ApplicationUser>>();
+            app.IdentityOptions = serviceProvider.GetRequiredService<IOptions<IdentityOptions>>();
+            app.ServiceProvider = serviceProvider;
+
+            return app;
+        }
+    }
+}
