225a578f37600ad6e16a9b364fde59d1bdc796e1
Alexey <eliseev-eag@yandex.ru>
2017-02-06 21:00:07 +0700

    Added PollingTask, CancelPolling and Dispose methods on AnalyticsAgent Added public methods to AnalyticsAgent which provides API to start task to recognition photo and cancel this task

--- Comptech.Backend/src/Comptech.Backend.Service/Analytics/AnalyticsAgent.cs
+++ Comptech.Backend/src/Comptech.Backend.Service/Analytics/AnalyticsAgent.cs
@@ -11,15 +11,16 @@ using System.Threading.Tasks;
 namespace Comptech.Backend.Service.Analytics
 {
     [Authorize]
-    public class AnalyticsAgent
+    public class AnalyticsAgent : IDisposable
     {
         private readonly RecognitionTaskQueue queue;
         private readonly IAnalyticsClient analyticsClient;
         private readonly ILogger logger;
         private readonly IConfiguration configuration;
         private readonly IPhotoRepository photoRepository;
+        private CancellationTokenSource cancellationTokenSource;
 
-        AnalyticsAgent(RecognitionTaskQueue queue,
+        public AnalyticsAgent(RecognitionTaskQueue queue,
                        IAnalyticsClient analyticsClient,
                        ILoggerFactory loggerFactory,
                        IPhotoRepository photoRepository,
@@ -32,20 +33,46 @@ namespace Comptech.Backend.Service.Analytics
             logger = loggerFactory.CreateLogger<AnalyticsAgent>();
         }
 
-        public async void Run()
+        public void PollingTask()
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+            Task.Run(() => Run(cancellationTokenSource.Token), cancellationTokenSource.Token);
+        }
+
+        public void CancelPolling()
+        {
+            cancellationTokenSource.Cancel();
+        }
+
+
+        public void Dispose()
+        {
+            CancelPolling();
+        }
+
+        private async void Run(CancellationToken token)
         {
             bool isInterrupted = false;
+            int pollingTimoutBetweenTasks = int.Parse(
+                configuration.GetSection("AnalyticsPollingTimeoutBetweenTasks").Value);
             while (!isInterrupted)
             {
                 try
                 {
                     RecognitionTask recognitionTask = queue.Dequeue();
                     await RunTaskForRecognition(recognitionTask);
+                    token.ThrowIfCancellationRequested();
+                }
+                catch (OperationCanceledException exception)
+                {
+                    logger.LogError("Tasks is cancelled");
+                    isInterrupted = true;
                 }
                 catch (Exception exception)
                 {
                     logger.LogError("Exception caught: {0}, {1}", exception.Message, exception.StackTrace);
                 }
+                Thread.Sleep(pollingTimoutBetweenTasks);
             }
         }
 
