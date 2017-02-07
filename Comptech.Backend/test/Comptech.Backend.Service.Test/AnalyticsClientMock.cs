a981fc57ac78df218478065280703b1d63f0dab4
Alexey <eliseev-eag@yandex.ru>
2017-02-07 12:26:40 +0700

    added test to ideal scenario

---
+++ Comptech.Backend/test/Comptech.Backend.Service.Test/AnalyticsClientMock.cs
@@ -0,0 +1,29 @@
+﻿using Comptech.Backend.Service.Analytics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Comptech.Backend.Data.DomainEntities;
+
+namespace Comptech.Backend.Service.Test
+{
+    public class AnalyticsClientMock : IAnalyticsClient
+    {
+        public Task<string> RequestRecognitionSession(string modelName)
+        {
+            return Task.Run(() => { return "aaa"; });
+        }
+
+        public Task<RecognitionResults> TryGetResults(string sessionUID, int photoId)
+        {
+            Points points = new Points(new Point(0, 0), new Point(10, 10));
+            RecognitionResults results = new RecognitionResults(true, points, 2);
+            return Task.Run(() => { return results; });
+        }
+
+        public Task UploadPhoto(byte[] photo, string sessionUID)
+        {
+            return Task.Run(() => { return; });
+        }
+    }
+}
---
+++ Comptech.Backend/test/Comptech.Backend.Service.Test/RecognitionResultsMock.cs
@@ -0,0 +1,29 @@
+﻿using Comptech.Backend.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Comptech.Backend.Data.DomainEntities;
+
+namespace Comptech.Backend.Service.Test
+{
+    public class RecognitionResultsMock : IRecognitionResultsRepository
+    {
+        private List<RecognitionResults> rep = new List<RecognitionResults>();
+        public bool Add(RecognitionResults entity)
+        {
+            rep.Add(entity);
+            return true;
+        }
+
+        public RecognitionResults GetRecognitionResultsByPhotoId(int photoId)
+        {
+            return rep.Last();
+        }
+
+        public bool Update(RecognitionResults entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
